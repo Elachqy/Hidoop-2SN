99532360f7562baa6103a32117a2ff910e708928
Younes Elachqar <youneselachqar@gmail.com>
2020-12-09 20:21:52 +0100

    tkherbi9a

--- src/hdfs/DataNode.java
+++ src/hdfs/DataNode.java
@@ -1,17 +1,35 @@
 package hdfs;
 
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DataNode {
     public int port;
-    public Map<String,Integer> listeBlocs; 
+    public Map<String, Integer> listeBlocs;
 
     public DataNode(int port) {
         this.port = port;
-        this.listeBlocs = new HashMap<String,Integer>(); 
+        this.listeBlocs = new HashMap<String, Integer>();
     }
 
+    public static void main(String[] args) {
+        // on commence par établir la connexion entre DataNode et NameNode
+        // récupération de l'adresse de la machine
+        try {
+        InetAddress adressemachine = InetAddress.getLocalHost(); // Inet4 vu qu'on utilise Ipv4
+        // récupération de l'adresse du namenode
+        InetAddress adresseNM = InetAddress.getByName(NameNode.NameNodeAdresse);
+        // on établit la connexion
+        String serveur; // path du serveur à insérer !!!!!!
+		Socket s1 = new Socket(serveur , NameNode.portNNDN);
+        }    
+
+
+    }
 
 
 
