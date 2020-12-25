7d8b5a3c893f61d51ca90a6eab1fa4d0ca2a6cbd
karimelhajjami <66040216+karimelhajjami@users.noreply.github.com>
2020-12-25 13:49:39 +0100

    Update HdfsClient.java
    HdfsWrite update

--- src/hdfs/HdfsClient.java
+++ src/hdfs/HdfsClient.java
@@ -5,11 +5,7 @@ import java.io.File;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Map;
-
-import formats.Format;
-import formats.KV;
-import formats.KVFormat;
-import formats.LineFormat;
+import formats.*;
 
 
 public class HdfsClient {
@@ -20,22 +16,22 @@ public class HdfsClient {
         System.out.println("Usage: java HdfsClient delete <file>");
     }
 	
-    public static void HdfsDelete(String hdfsFname) {
-        //recherche des blocs du fichier à supprimer
-        
-        // mapnode du fichier à supp ( à récupérer du serveur)
+    public static void HdfsDelete(String FileName) {
+	    
+        /* MapNode du fichier à supprimer (à récupérer du serveur) */
         Map<String,String> mapnode_fichier; //à compléter
-        // liste des machines où un bloc du fichier existe(values de la mapnode du fichier)
+	    
+        // liste des machines où un bloc du fichier existe (values de la mapnode du fichier)
         ArrayList<String> listemachines = new ArrayList<String>(mapnode_fichier.values());
         if (listemachines == null) {
             System.out.println("Fichier inexistant!");
         }
-        else{ //on supprime les bouts du fichier dans les différents datanode
+        else{ /* on supprime les bouts du fichier dans les différents datanode */
           for (String machine : listemachines) {
-                String[] x = machine.split("@"); //on récup les coordonnées de la machine
-                String adresse = x[0];
-                int port = Integer.parseInt(x[1]);
-                //HdfsServeur.delete(etc etc)
+                String[] coordonnees = machine.split("@"); //on récupère les coordonnées de la machine
+                String adresse = coordonnees[0];
+                int port = Integer.parseInt(coordonnees[1]);
+                //HdfsServeur.delete(...)
             }
            // à compléter
         } 
@@ -44,10 +40,79 @@ public class HdfsClient {
     
     }
 	
-    public static void HdfsWrite(Format.Type fmt, String localFSSourceFname, 
-     int repFactor) { 
-
-     }
+    public static void HdfsWrite(Format.Type fmt, String localFSSourceFname) { 
+	    Format FileFmt; 	// le format du fichier à envoyer
+	    //File fichier = new File(...) récupération du fichier du système local (à compléter)
+	    if (fichier.exists()) {
+	    	long taille = fichier.length()*8; 	// la taille du fichier en bits
+	    
+	    	/* Détermination de la taille d'un fragment : */
+	    	long tailleFragment;
+	    
+	    	if (taille <= 1 * (long)(Math.pow(10, 6)) * 8) { /* taille <= 1 Mo */
+			tailleFragment = 100 * (long)(Math.pow(10, 3)) * 8;	//la taille d'un fragment est égal à 100 Ko
+		    
+	    	} else if ( 1 * (long)(Math.pow(10, 6)) * 8 < taille && taille <= 30 * (long)(Math.pow(10, 6)) * 8) { /* 1 Mo <= taille <= 30 Mo */
+			tailleFragment = 900 * (long)(Math.pow(10, 3)) * 8;	//la taille d'un fragment est égal à 900 Ko
+	    
+	    	} else if (30 * (long)(Math.pow(10, 6)) * 8 < taille && taille <= 150 * (long)(Math.pow(10, 6)) * 8){ /* 30 Mo < taille <= 150 Mo */
+			tailleFragment = 5 * (long)(Math.pow(10, 6)) * 8;	//la taille d'un fragment est égal à 5 Mo
+	    
+	    	} else if (150 * (long)(Math.pow(10, 6)) * 8 < taille && taille <= 600 * (long)(Math.pow(10, 6)) * 8){ /* 150 Mo < taille <= 600 Mo */
+			tailleFragment = 32 * (long)(Math.pow(10, 6)) * 8;	//la taille d'un fragment est égal à 32 Mo
+	    
+	    	} else if (600 * (long)(Math.pow(10, 6)) * 8 < taille && taille <= 1500 * (long)(Math.pow(10, 6)) * 8){ /* 600 Mo < taille <= 1.5 Gb */
+			tailleFragment = 64 * (long)(Math.pow(10, 6)) * 8;	//la taille d'un fragment est égal à 64 Mo
+	    
+	    	} else if (1500 * (long)(Math.pow(10, 6)) * 8 < taille && taille <= 6000 * (long)(Math.pow(10, 6)) * 8){ /* 1.5 Gb  < taille <= 6 Gb */
+			tailleFragment = 100 * (long)(Math.pow(10, 6)) * 8;	//la taille d'un fragment est égal à 100 Mo
+	    
+	    	} else {
+			tailleFragment = 130 * (long)(Math.pow(10, 6)) * 8;	//sinon la taille d'un fragment est égal à 130 Mo
+	    	}	
+	    
+	    	/* Calcul du nombre de fragments : */
+	    	int nbFragments = 0;
+	    	if (fmt == Format.Type.LINE) {
+			    FileFmt = new LineFormat(...); //à compléter (Projet.fichierSource?)
+	    	} else {
+			    FileFmt = new KVFormat(...); //à compléter (Projet.fichierSource?)
+	    	}
+	    	FileFmt.open(Format.OpenMode.R);
+	    	KV FileKV = FileFmt.read();
+	    	long compteur;
+	    	boolean continuer = true;
+	    	while(continuer) {
+			    if (FileKV == null) {
+				    continuer = false;
+		    	} else {
+				    while(compteur<tailleFragment) {
+					    FileKV = file.read();
+				    	if (FileKV == null) {
+					        continuer = false;
+					    	break;
+				    	} else if (fmt == Format.Type.LINE) {
+					    	compteur += FileKV.v.length()*8;
+				    	} else {
+					    	compteur += (FileKV.k + KV.SEPARATOR + FileKV.v).length()*8;
+				    	}
+			     	}   
+			     	nbFragments++
+			     	compteur = 0;	// on rinitialise à 0 après chaque fragment compté
+		    	}
+	     	}
+	     	FileFmt.close();
+	    	/* Récupération de la liste des DataNode où on va envoyer les fragments sous la forme "adress@port" */
+		// à compléter
+		/* Lancer un thread pour écrire les fragments dans les DataNodes */
+		// à compléter
+		// ...
+		    
+	   } else {
+		    fichier.mkdir();
+		    System.out.println("Fichier inexistant!");
+	   }
+    }
 
     public static void HdfsRead(String hdfsFname, String localFSDestFname) {
      // récupération du fichier 
